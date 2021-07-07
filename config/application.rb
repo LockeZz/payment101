b6c5f7382a0d68d9babc352aea7e6e691998e0dd
LockeZz <LockeZz.PHF@gmail.com>
2021-07-07 12:29:58 +0800

    added env variable

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Payment101
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config_file = Rails.application.config_for(:application)
+    config_file.each do |key, value|
+      ENV[key] = value
+    end unless config_file.nil?
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
